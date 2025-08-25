66d7e75be084179c6bbdc5b172cd1168de2b1bcb
ertan123456 <erdemertan08@gmail.com>
2025-08-25 21:00:01 +0300

    Initial commit with project files

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,7 +2,7 @@
 import os
 
 # Render'ın otomatik sağladığı PORT
-port = os.environ.get("PORT", 8000)
+port = os.environ.get("PORT", 10000)
 bind = f"0.0.0.0:{port}"
 
 # FastAPI için optimize worker ayarları
